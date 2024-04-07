9c59be357aaaa8a2cce5791632d656661ca43a5e
PedroHAMota <henrypedrinho11@gmail.com>
2024-04-07 18:09:09 -0300

    commit tela login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,12 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+
 ];
 
 @NgModule({
