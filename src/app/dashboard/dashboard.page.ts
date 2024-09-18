7b7a0c270453cbd2e77c6778d31dbee2aa6a5691
PedroHAMota <henrypedrinho11@gmail.com>
2024-09-18 17:27:53 -0300

    commit dashboard cards

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -12,4 +12,26 @@ export class DashboardPage implements OnInit {
   ngOnInit() {
   }
 
+  public chartData = {
+    datasets: [
+      {
+        label: 'Vendas',
+        data: [10,20,30,40,50],
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }
+    ],
+    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+  };
+
+  public chartOptions = {
+    responsive: true,
+    scales:{
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+
 }
