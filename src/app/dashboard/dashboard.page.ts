3f8f98bf8b1d9c4baf924909f882caae55d55552
PedroHAMota <henrypedrinho11@gmail.com>
2024-10-01 14:37:28 -0300

    commit 1

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart, ChartConfiguration, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,29 +10,58 @@ export class DashboardPage implements OnInit {
 
   constructor() { }
 
+  @ViewChild("meuCanvas", { static: true }) elemento!: ElementRef;
+
   ngOnInit() {
+
+    const chartConfig: ChartConfiguration = {
+      type: 'line' as ChartType, // Especifica o tipo de gráfico como 'line'
+      data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'], // Etiquetas para o eixo X
+        datasets: [
+          {
+            label: 'Vendas',
+            data: [10, 20, 30, 40], // Dados a serem exibidos
+            backgroundColor: 'rgba(75, 192, 192, 0.2)', // Cor de fundo
+            borderColor: 'rgba(75, 192, 192, 1)', // Cor da borda
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        responsive: true, // Faz o gráfico ser responsivo
+        scales: {
+          y: {
+            beginAtZero: true // Define o eixo Y para começar no zero
+          }
+        }
+      }
+    };
+
+    new Chart(this.elemento.nativeElement, chartConfig);
+
   }
 
-  public chartData = {
-    datasets: [
-      {
-        label: 'Vendas',
-        data: [10,20,30,40,50],
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1
-      }
-    ],
-    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-  };
+  // public chartData = {
+  //   datasets: [
+  //     {
+  //       label: 'Vendas',
+  //       data: [10,20,30,40,50],
+  //       backgroundColor: 'rgba(54, 162, 235, 0.2)',
+  //       borderColor: 'rgba(54, 162, 235, 1)',
+  //       borderWidth: 1
+  //     }
+  //   ],
+  //   labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+  // };
 
-  public chartOptions = {
-    responsive: true,
-    scales:{
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
+  // public chartOptions = {
+  //   responsive: true,
+  //   scales:{
+  //     y: {
+  //       beginAtZero: true
+  //     }
+  //   }
+  // }
 
 }
