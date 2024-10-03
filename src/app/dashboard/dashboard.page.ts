31574d2dbb425da9a1b1f4fba912fbbc46796141
PedroHAMota <henrypedrinho11@gmail.com>
2024-10-03 09:07:44 -0300

    commit1

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-// import { Chart, ChartConfiguration, ChartType } from 'chart.js';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -14,46 +15,32 @@ export class DashboardPage implements OnInit {
 
   ngOnInit() {
 
-    // const chartConfig: ChartConfiguration = {
-    //   type: 'bar' as ChartType, // Especifica o tipo de gráfico como 'line'
-    //   data: {
-    //     labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril'], // Etiquetas para o eixo X
-    //     datasets: [
-    //       {
-    //         label: 'Vendas',
-    //         data: [10, 20, 30, 40], // Dados a serem exibidos
-    //         backgroundColor: 'rgba(75, 192, 192, 0.2)', // Cor de fundo
-    //         borderColor: 'rgba(75, 192, 192, 1)', // Cor da borda
-    //         borderWidth: 1
-    //       }
-    //     ]
-    //   },
-    //   options: {
-    //     responsive: true, // Faz o gráfico ser responsivo
-    //     scales: {
-    //       y: {
-    //         beginAtZero: true // Define o eixo Y para começar no zero
-    //       }
-    //     }
-    //   }
-    // };
-
-    // new Chart(this.elemento.nativeElement, chartConfig);
+    const config = {
+      type: 'bar',
+      data: ['Jan','Fev','Mar', 'Abr', 'Mar', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    };
 
   }
 
-  // public chartData = {
-  //   datasets: [
-  //     {
-  //       label: 'Vendas',
-  //       data: [10,20,30,40,50],
-  //       backgroundColor: 'rgba(54, 162, 235, 0.2)',
-  //       borderColor: 'rgba(54, 162, 235, 1)',
-  //       borderWidth: 1
-  //     }
-  //   ],
-  //   labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-  // };
+  //  public chartData = {
+  //    datasets: [
+  //      {
+  //        label: 'Vendas',
+  //        data: [10,20,30,40,50],
+  //        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+  //        borderColor: 'rgba(54, 162, 235, 1)',
+  //        borderWidth: 1
+  //      }
+  //    ],
+  //    labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+  //  };
 
   // public chartOptions = {
   //   responsive: true,
