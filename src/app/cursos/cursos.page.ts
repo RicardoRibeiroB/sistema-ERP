4e53b07de760984af55e0093fb1d96b17ab36715
RicardoRibeiroB <ricardoribeiro062106@gmail.com>
2024-10-30 16:54:41 -0300

    endpoits cursos

--- src/app/cursos/cursos.page.ts
+++ src/app/cursos/cursos.page.ts
@@ -1,215 +1,66 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import Swiper from 'swiper';
 
+interface CourseCard {
+  id: number;
+  title: string;
+  institute: string;
+  rating: number;
+  image: string;
+  price: number;
+  category: string;
+  type: 'premium' | 'free';  
+}
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.page.html',
   styleUrls: ['./cursos.page.scss'],
 })
 export class CursosPage implements OnInit, AfterViewInit {
-  cards_premium = [
-    {
-      id: 1,
-      title: 'Curso de Programação',
-      institute: 'Instituto de Tecnologia',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.99,
-      category: 'Programação'
-    },
-    {
-      id: 2,
-      title: 'Curso de Design Gráfico',
-      institute: 'Escola de Artes Visuais',
-      rating: 4,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 150.00,
-      category: 'Design Gráfico'
-    },
-    {
-      id: 3,
-      title: 'Curso de Marketing Digital',
-      institute: 'Universidade de Negócios',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 299.99,
-      category: 'Marketing Digital'
-    },
-    {
-      id: 4,
-      title: 'Curso de Fotografia',
-      institute: 'Escola de Fotografia',
-      rating: 4,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 250.00,
-      category: 'Fotografia'
-    },
-    {
-      id: 5,
-      title: 'Curso de Desenvolvimento Web',
-      institute: 'Instituto de Tecnologia',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 299.99,
-      category: 'Desenvolvimento Web'
-    },
-    {
-      id: 6,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 7,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 8,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    }
-  ];
-  cards_free = [
-    {
-      id: 1,
-      title: 'Curso de Programação',
-      institute: 'Instituto de Tecnologia',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.99,
-      category: 'Programação'
-    },
-    {
-      id: 2,
-      title: 'Curso de Design Gráfico',
-      institute: 'Escola de Artes Visuais',
-      rating: 4,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 150.00,
-      category: 'Design Gráfico'
-    },
-    {
-      id: 3,
-      title: 'Curso de Marketing Digital',
-      institute: 'Universidade de Negócios',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 299.99,
-      category: 'Marketing Digital'
-    },
-    {
-      id: 4,
-      title: 'Curso de Fotografia',
-      institute: 'Escola de Fotografia',
-      rating: 4,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 250.00,
-      category: 'Fotografia'
-    },
-    {
-      id: 5,
-      title: 'Curso de Desenvolvimento Web',
-      institute: 'Instituto de Tecnologia',
-      rating: 5,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 299.99,
-      category: 'Desenvolvimento Web'
-    },
-    {
-      id: 6,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 7,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 8,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 9,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 10,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 11,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    },
-    {
-      id: 12,
-      title: 'Curso de Gestão de Projetos',
-      institute: 'Faculdade de Negócios',
-      rating: 3,
-      image: '../../assets/img/Tela-Cursos/imagem-curso-exemplo.jpeg',
-      price: 199.00,
-      category: 'Gestão de Projetos'
-    }
-  ];
-  
-  
+
+  cards_free: CourseCard[] = [];
+  cards_premium: CourseCard[] = [];
 
   constructor() {}
 
   ngOnInit() {
-    console.log(this.cards_premium);
+    this.loadCourses();
   }
 
   ngAfterViewInit() {
     const swiper = new Swiper('.swiper-container', {
-      slidesPerView: 'auto', 
+      slidesPerView: 'auto',
       spaceBetween: 10,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
     });
-
     console.log('Swiper inicializado:', swiper);
+  }
+
+  loadCourses() {
+    fetch('http://localhost/api/cursos', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then((response: CourseCard[]) => {
+      response.forEach(course => {
+        if (course.type === 'free') {
+          this.cards_free.push(course);
+        } else if (course.type === 'premium') {
+          this.cards_premium.push(course);
+        }
+      });
+      console.log('Cursos gratuitos:', this.cards_free);
+      console.log('Cursos premium:', this.cards_premium);
+    })
+    .catch(error => {
+      console.error('Erro ao carregar cursos:', error);
+    });
   }
 }
