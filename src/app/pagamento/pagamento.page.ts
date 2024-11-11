7685ea79a1a73858f926be8c771d16617bd918b7
RicardoRibeiroB <ricardoribeiro062106@gmail.com>
2024-11-11 15:11:37 -0300

    tela pagamento

--- src/app/pagamento/pagamento.page.ts
+++ src/app/pagamento/pagamento.page.ts
@@ -12,4 +12,31 @@ export class PagamentoPage implements OnInit {
   ngOnInit() {
   }
 
+  isModalOpen: boolean = false;
+  selectedPayment: string | null = null;
+  isPixSelected: boolean = false;
+  isCardSelected: boolean = false;
+
+  selectPayment(paymentType: string) {
+    if (paymentType === 'pix') {
+      this.isPixSelected = !this.isPixSelected;
+      this.isCardSelected = false;
+      this.selectedPayment = this.isPixSelected ? 'pix' : null;
+    } else if (paymentType === 'card') {
+      this.isCardSelected = !this.isCardSelected;
+      this.isPixSelected = false;
+      this.selectedPayment = this.isCardSelected ? 'card' : null;
+    }
+
+    // Abre o modal apenas se uma opção estiver selecionada
+    this.isModalOpen = this.selectedPayment !== null;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedPayment = null;
+    this.isPixSelected = false;
+    this.isCardSelected = false;
+  }
+
 }
