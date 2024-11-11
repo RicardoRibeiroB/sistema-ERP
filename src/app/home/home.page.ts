9b5f9ea6c7d6d46923a058a1c0b02032595cc162
PedroHAMota <henrypedrinho11@gmail.com>
2024-11-11 15:16:49 -0300

    commit link page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
 
   constructor() {}
+
+  scrollToPlans() {
+    const element = document.getElementById('planos');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToAbout() {
+    const element = document.getElementById('sobre');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToInicio() {
+    const element = document.getElementById('inicio');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToEstudeConosco() {
+    const element = document.getElementById('estude-conosco');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  scrollToDivulgue() {
+    const element = document.getElementById('divulgue');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 
 }
